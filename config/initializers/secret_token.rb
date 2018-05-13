a6c5af885fac002595607ab60f86114370fd0eb2
Susan Jacob <susan@stayntouch.com>
2018-05-13 15:14:55 +0530

    Create the project travelogue

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelogue::Application.config.secret_token = 'f9b4748a7e562f8ff52d085de84f3cdbd4281b6546f8c959289ef7973e7a7ab2eea4c2dea3b451186d58d0f9b584951e7a37e04136eac5c1f2286f257acbcc72'
